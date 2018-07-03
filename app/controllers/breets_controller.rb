0ef6e64141106f6bd76a167e56a66e5cfe9f0168
Jonathan Momperousse <bklynbest718@gmail.com>
2018-07-03 17:42:55 +0000

    updated views for devise with bulma

--- app/controllers/breets_controller.rb
+++ app/controllers/breets_controller.rb
@@ -1,5 +1,6 @@
 class BreetsController < ApplicationController
   before_action :set_breet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /breets
   # GET /breets.json
